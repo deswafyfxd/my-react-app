73156c3ecf21d02fe984f569827a2ca9881fd668
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-08 22:18:27 +0530

    Create App.js

---
+++ outlook-management-ui/public/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AccountForm from './components/AccountForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Outlook Account Management</h1>
+        <AccountForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
