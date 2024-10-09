771a8926fc4c8bea227b004fd9078953a178ba9a
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-09 16:43:15 +0530

    Create index.js

---
+++ outlook-management-ui/src/components/VideoPlayer/index.js
@@ -0,0 +1,6 @@
+import VideoPlayer from './VideoPlayer';
+import Subtitles from './Subtitles';
+import AudioTracks from './AudioTracks';
+import Controls from './Controls';
+
+export { VideoPlayer, Subtitles, AudioTracks, Controls };
