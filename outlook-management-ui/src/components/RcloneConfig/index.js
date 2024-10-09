bc89f5c86588dc45975e2e9106281c96992e4f49
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-09 16:34:00 +0530

    Update index.js

--- outlook-management-ui/src/components/RcloneConfig/index.js
+++ outlook-management-ui/src/components/RcloneConfig/index.js
@@ -1,16 +1,6 @@
-import React from 'react';
 import ConfigForm from './ConfigForm';
 import MountForm from './MountForm';
 import SyncForm from './SyncForm';
 import ServeForm from './ServeForm';
 
-const RcloneConfig = () => (
-  <div>
-    <ConfigForm />
-    <MountForm />
-    <SyncForm />
-    <ServeForm />
-  </div>
-);
-
-export default RcloneConfig;
+export { ConfigForm, MountForm, SyncForm, ServeForm };
