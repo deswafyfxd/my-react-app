bf134e0ccb2d3fd1ac3512cb1784a605a51cc2a2
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-09 18:23:26 +0530

    Update VideoPlayer.js

--- outlook-management-ui/src/components/VideoPlayer/VideoPlayer.js
+++ outlook-management-ui/src/components/VideoPlayer/VideoPlayer.js
@@ -10,7 +10,7 @@ const VideoPlayer = ({ src, subtitles, audioTracks, videoFormats }) => {
   }, []);
 
   return (
-    <div>
+    <div className="video-container">
       <video id="my-video" className="video-js" controls preload="auto" width="100%" height="auto" data-setup="{}">
         {videoFormats.map((format, index) => (
           <source key={index} src={format.src} type={format.type} />
