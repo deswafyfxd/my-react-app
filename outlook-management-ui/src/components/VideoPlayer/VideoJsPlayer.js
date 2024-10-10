282dfbfbfd86ca98adb5ef66ae21be963ceec032
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-10 16:27:23 +0530

    Update VideoJsPlayer.js

--- outlook-management-ui/src/components/VideoPlayer/VideoJsPlayer.js
+++ outlook-management-ui/src/components/VideoPlayer/VideoJsPlayer.js
@@ -1,18 +1,26 @@
 import React, { useEffect } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
+import { addSubtitles } from './subtitles';
+import { addAudioTracks } from './AudioTracks';
 
-const VideoJsPlayer = ({ src }) => {
+const VideoJsPlayer = ({ src, subtitles, audioTracks }) => {
   useEffect(() => {
     const player = videojs('video-js-player', {
       controls: true,
       autoplay: false,
       preload: 'auto'
     });
+
+    player.on('ready', () => {
+      addSubtitles(player, subtitles);
+      addAudioTracks(player, audioTracks);
+    });
+
     return () => {
       player.dispose();
     };
-  }, [src]);
+  }, [src, subtitles, audioTracks]);
 
   return (
     <div className="video-container">
